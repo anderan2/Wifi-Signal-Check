19b523be350fd58807a3d7ecb8232ef88a59ec20
anderan2 <31325700+anderan2@users.noreply.github.com>
2020-08-20 20:22:18 -0700

    Add files via upload

--- signal-check.py
+++ signal-check.py
@@ -1,11 +1,13 @@
 import time
 import os
+import json
+from get_wifi_info import read_write_output
 
 """******************************************************************
 Author: 			Andrew A. Anderson
 Date Made:			8/20/2020
 Date Mod:			8/20/2020
-Release:			V1.0.0
+Release:			V1.1.0
 
 Purpose:			I built this programme because I got annoyed with
 					my ISP saying that there was nothing wrong on
@@ -24,42 +26,41 @@ Modifications:		Made the read_write_output function for
 If you run into any issues, please contact me via github.
 ******************************************************************"""
 
+#def read_write_output(display, file_in, file_out ):
+#	with open( file_in, 'r' ) as read:
+#		lines = read.readlines()
 
-def read_write_output(display, file_in, file_out ):
-	with open( file_in, 'r' ) as read:
-		lines = read.readlines()
+#	for line in lines:
 
-	for line in lines:
-
-		if "Transmit rate" in line:
-			if line[ -3:-2 ] == ".":
-				transmit = line[ -6:-1].strip()
-				#print( transmit )
-			else:
-				if line[ -4:-3 ].rstrip() != ":":
-					transmit = line[ -5: ].strip()
-					#print( transmit )
-				else:
-					transmit = line[ -2: ].strip()
-					#print( transmit )
+#		if "Transmit rate" in line:
+#			if line[ -3:-2 ] == ".":
+#				transmit = line[ -6:-1].strip()
+#				#print( transmit )
+#			else:
+#				if line[ -4:-3 ].rstrip() != ":":
+#					transmit = line[ -5: ].strip()
+#					#print( transmit )
+#				else:
+#					transmit = line[ -2: ].strip()
+#					#print( transmit )
 
 
-		if "Receive rate" in line:
-			if line[ -3:-2 ] == ".":
-				receive = line[ -6: ].strip()
-			else:
-				if line[ -4:-3 ].rstrip() != ":":
-					receive = line[ -5: ].strip()
-				else:
-					receive = line[ -2: ].strip()
+#		if "Receive rate" in line:
+#			if line[ -3:-2 ] == ".":
+#				receive = line[ -6: ].strip()
+#			else:
+#				if line[ -4:-3 ].rstrip() != ":":
+#					receive = line[ -5: ].strip()
+#				else:
+#					receive = line[ -2: ].strip()
 			#print( receive )
-		if "Signal" in line:
-			strength = line[-5:-3].strip()
+#		if "Signal" in line:
+#			strength = line[-5:-3].strip()
 
-	with open( file_out, 'a' ) as write:
+#	with open( file_out, 'a' ) as write:
 
-		write.write( f"{display},{strength},{receive},{transmit}\n")
-		write.close()
+#		write.write( f"{display},{strength},{receive},{transmit}\n")
+#		write.close()
 
 
 
